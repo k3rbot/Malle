944ebc2917d6d95cd2da8e894c8846d5f30922c7
PITON Martin <PITON@0440021j.paysdelaloire.education>
2021-12-03 10:21:57 +0100

    Docstring added

--- Malle_1_PITON_RAVILY_BARAUD-MYSKO.py
+++ Malle_1_PITON_RAVILY_BARAUD-MYSKO.py
@@ -1,6 +1,16 @@
 from random import randint
 
-def Fleury_and_bott(monnaie):
+def Fleury_and_bott(monnaie: int) -> dict:
+    """
+    Fonction permettant de savoir le nombre de
+    billets/pièces à rendre pour un montant donné
+
+    Entrée: Montant à rendre
+    Sortie: Dictionnaire comprenant la valeur de
+            le pièce/du billet et le nombre à rendre 
+    """
+    assert type(monnaie) == int, "Vous devez rentrer un nombre conforme"
+
     monnaie_dispo = (500, 200, 100, 50, 20, 10, 5, 2, 1)
     monnaie_rendue = {500 : 0, 200 : 0, 100 : 0, 50 : 0,
                     20 : 0, 10 : 0, 5 : 0, 2 : 0, 1 : 0}
