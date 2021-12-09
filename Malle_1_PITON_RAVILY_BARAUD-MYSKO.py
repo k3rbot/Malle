5a61f4d737a85dd4aec1476d387364eb49e0df32
PITON Martin <PITON@0440021j.paysdelaloire.education>
2021-12-09 15:31:34 +0100

    Update interface

--- Malle_1_PITON_RAVILY_BARAUD-MYSKO.py
+++ Malle_1_PITON_RAVILY_BARAUD-MYSKO.py
@@ -1,7 +1,39 @@
-from random import randint
+import os
 import pygame as pg
 
 pg.init()
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"  # On centre la fenêtre PyGame
+# On initialise la fenêtre de 800 par 600 pixels
+screen = pg.display.set_mode((0, 0), pg.FULLSCREEN, pg.RESIZABLE)
+
+# On définit plusieurs couleurs au format rgb
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (50, 50, 50)
+red = (255, 0, 0)
+green = (34, 139, 34)
+violet = (128, 0, 128)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+golden = (212, 175, 55)
+silver = (192, 192, 192)
+bronze = (205, 127, 50)
+orange = (255, 140, 0)
+
+clock = pg.time.Clock()
+FPS = 30
+# On utilise une police d"écriture spécifique
+font = "HarryPotterFont.ttf"
+pg.display.update()
+# On définit la vitesse de rafraichissement à 30 FPS
+clock.tick(FPS)
+# On définit le titre de la fenêtre
+pg.display.set_caption("HARRY POTTER SE FAITY LA MALLE")
+# On affiche l'image du chemin de traverse
+img_chemin = pg.image.load("chemin_de_traverse.png")
+screen.blit(img_chemin, (0, 0))
+pg.display.update()
 
 def Fleury_and_bott(monnaie: int) -> dict:
     """
@@ -29,3 +61,4 @@ print(Fleury_and_bott(60))
 print(Fleury_and_bott(63))
 print(Fleury_and_bott(231))
 print(Fleury_and_bott(899))
+pg.time.wait(10000)
