2942749a02fdea1c35d56706c8384e12eff64094
Bambinraph <95035933+Bambinraph@users.noreply.github.com>
2021-12-16 22:21:32 +0100

    Update Malle_1_PITON_RAVILY_BARAUD-MYSKO.py

--- Malle_1_PITON_RAVILY_BARAUD-MYSKO.py
+++ Malle_1_PITON_RAVILY_BARAUD-MYSKO.py
@@ -125,30 +125,27 @@ print(flourish_and_blotts(231))
 print(flourish_and_blotts(899))
 """
 
-def malkin(rendu:int) -> dict:
-    assert type(rendu) == int
-
-    caisse_dispo = {200 : 1, 100 : 3, 50 : 1 , 20 : 1, 10: 1, 2 : 5}
-    rendu_caisse ={200 : 0, 100 : 0, 50 : 0, 20 : 0, 10: 0, 2 : 0}
-    
-    for thune in caisse_dispo:
-        while rendu >= thune:
-                if caisse_dispo[thune] > 0:
-                    rendu -= thune
-                    rendu_caisse[thune] += 1
-                else:
-                    break 
-                
-    return rendu_caisse
-
-"""
-print(malkin(0))
-print(malkin(8))
-print(malkin(62))
-print(malkin(231))
-print(malkin(497))
-print(malkin(842))
-"""
+#def malkin(rendu:int) -> dict:
+#    assert type(rendu) == int
+#
+#    caisse_dispo = {200: 1, 100 : 3, 50 : 1 ,  20: 1, 10: 1, 2 : 5}
+#    rendu_caisse = {200 : 0, 100 : 0, 50 : 0, 20 : 0, 10: 0, 2 : 0}
+#    
+#    for thune in caisse_dispo :
+#        while rendu >= thune  :
+#                if rendu_caisse != caisse_dispo :
+#                    rendu -= thune
+#                    rendu_caisse[thune] += 1
+#                else:
+#                    break 
+#                
+#    return rendu_caisse
+#print(malkin(0))
+#print(malkin(8))
+#print(malkin(62))
+#print(malkin(231))
+#print(malkin(497))
+#print(malkin(842))
 
 def ollivander(amount:list):
     """
