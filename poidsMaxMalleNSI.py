e3bab72a1fa5fc334027ef9cfe706a2218be9f9a
Melthem <95036441+Melthem@users.noreply.github.com>
2022-01-21 09:56:10 +0100

    Add files via upload
    Added Max weight trunk solution, maybe not the best way mais ça marche tkt martin me tue pas

---
+++ poidsMaxMalleNSI.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 21 09:22:23 2022
+
+@author: BARAUD--MYSKO
+"""
+
+
+fournitures_scolaires = \
+[{'Nom' : 'Manuel scolaire', 'Poids' : 0.55, 'Mana' : 11},
+{'Nom' : 'Baguette magique', 'Poids' : 0.085, 'Mana' : 120},
+{'Nom' : 'Chaudron', 'Poids' : 2.5, 'Mana' : 2},
+{'Nom' : 'Boîte de fioles', 'Poids' : 1.2, 'Mana' : 4},
+{'Nom' : 'Téléscope', 'Poids' : 1.9, 'Mana' : 6},
+{'Nom' : 'Balance de cuivre', 'Poids' : 1.3, 'Mana' : 3},
+{'Nom' : 'Robe de travail', 'Poids' : 0.5, 'Mana' : 8},
+{'Nom' : 'Chapeau pointu', 'Poids' : 0.7, 'Mana' : 9},
+{'Nom' : 'Gants', 'Poids' : 0.6, 'Mana' : 25},
+{'Nom' : 'Cape', 'Poids' : 1.1, 'Mana' : 13}]
+
+
+
+def remplissage_max(fournitures):
+    
+    poids_liste = []
+    for element in fournitures:
+        poids_liste.append(element["Poids"])
+    
+
+    for i in range (1, len(poids_liste)):
+        while poids_liste[i] < poids_liste[i - 1] and i > 0:
+            poids_liste[i], poids_liste[i - 1] = poids_liste[i - 1], poids_liste[i]
+            i = i - 1
+        
+
+    somme_poids = 0
+    liste_poids = []
+    for i in poids_liste:
+        somme_poids += i
+        liste_poids.append(i)
+        if somme_poids >= 4:
+            somme_poids -= i
+            liste_poids
+            break
+
+    malle_max = []
+    for element in fournitures_scolaires:
+        for poids in liste_poids:
+            if poids == element["Poids"]:
+                malle_max.append(element)
+    
+    return(malle_max)
+
+print(remplissage_max(fournitures_scolaires))
