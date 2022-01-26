e17c4988eec2d658b6e27c971f44b5cf61a3d219
k3rbot <marty.piton@gmail.com>
2022-01-26 17:04:24 +0100

    Renamed pseudo code file of part 1 and added one for part 2

--- Malle_2_PITON_RAVILY_BARAUD-MYSKO.py
+++ Malle_2_PITON_RAVILY_BARAUD-MYSKO.py
@@ -246,7 +246,7 @@ def flourish_and_blotts(monnaie: int) -> dict:
     return monnaie_rendue
 
 
-def malkin(rendu: int) -> dict:
+def malkin(monnaie: int) -> dict:
     """
     Fonction permettant de savoir le nombre de
     billets/pièces minimum à rendre pour un montant donné
@@ -258,16 +258,16 @@ def malkin(rendu: int) -> dict:
             ! Si la valeur est trop élevée la fonction ne
             renverra rien
     """
-    assert type(rendu) == int
+    assert type(monnaie) == int
 
     monnaie_dispo = {200: 1, 100: 3, 50: 1, 20: 1, 10: 1, 2: 5}
     rendu_caisse = {200: 0, 100: 0, 50: 0, 20: 0, 10: 0, 2: 0, "impossible": False}
     for billet in monnaie_dispo:
-            while rendu >= billet and monnaie_dispo[billet] > 0:
+            while monnaie >= billet and monnaie_dispo[billet] > 0:
                 monnaie_dispo[billet] -= 1
-                rendu -= billet
+                monnaie -= billet
                 rendu_caisse[billet] += 1
-    if rendu > 0:
+    if monnaie > 0:
         rendu_caisse["impossible"] = True
     return rendu_caisse
 
