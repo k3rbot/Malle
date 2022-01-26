f710e1560feb516328848cddb150ba165b6bb496
k3rbot <marty.piton@gmail.com>
2022-01-26 16:45:44 +0100

    Added brute force method

--- Malle_2_PITON_RAVILY_BARAUD-MYSKO.py
+++ Malle_2_PITON_RAVILY_BARAUD-MYSKO.py
@@ -1,3 +1,4 @@
+from msilib import Binary
 import os
 import pygame as pg
 
@@ -104,18 +105,19 @@ FLOURISH_AND_BLOTTS_TESTS = ('0', '60', '63', '231', '899')
 MALKIN_TESTS = ('0', '8', '62', '231', '497', '842')
 
 # Les fournitures pour la malle d'Harry
-fournitures_scolaires = [
-    {'Nom' : 'Manuel scolaire', 'Poids' : 0.55, 'Mana' : 11},
-    {'Nom' : 'Baguette magique', 'Poids' : 0.085, 'Mana' : 120},
-    {'Nom' : 'Chaudron', 'Poids' : 2.5, 'Mana' : 2},
-    {'Nom' : 'Boîte de fioles', 'Poids' : 1.2, 'Mana' : 4},
-    {'Nom' : 'Téléscope', 'Poids' : 1.9, 'Mana' : 6},
-    {'Nom' : 'Balance de cuivre', 'Poids' : 1.3, 'Mana' : 3},
-    {'Nom' : 'Robe de travail', 'Poids' : 0.5, 'Mana' : 8},
-    {'Nom' : 'Chapeau pointu', 'Poids' : 0.7, 'Mana' : 9},
-    {'Nom' : 'Gants', 'Poids' : 0.6, 'Mana' : 25},
-    {'Nom' : 'Cape', 'Poids' : 1.1, 'Mana' : 13}
+SCHOLAR_FURNITURES = [
+    {'Nom': 'Manuel scolaire', 'Poids': 0.55, 'Mana': 11},
+    {'Nom': 'Baguette magique', 'Poids': 0.085, 'Mana': 120},
+    {'Nom': 'Chaudron', 'Poids': 2.5, 'Mana': 2},
+    {'Nom': 'Boîte de fioles', 'Poids': 1.2, 'Mana': 4},
+    {'Nom': 'Téléscope', 'Poids': 1.9, 'Mana': 6},
+    {'Nom': 'Balance de cuivre', 'Poids': 1.3, 'Mana': 3},
+    {'Nom': 'Robe de travail', 'Poids': 0.5, 'Mana': 8},
+    {'Nom': 'Chapeau pointu', 'Poids': 0.7, 'Mana': 9},
+    {'Nom': 'Gants', 'Poids': 0.6, 'Mana': 25},
+    {'Nom': 'Cape', 'Poids': 1.1, 'Mana': 13}
 ]
+MAX_WEIGHT = 4
 
 
 def display_text(text: str, font: list, size: int, color: tuple, x: int, y: int, alignment=1):
@@ -144,6 +146,83 @@ def display_text(text: str, font: list, size: int, color: tuple, x: int, y: int,
         screen.blit(text, (x - (text_rect[2]/2), y))
     else:  # Alignement à droite
         screen.blit(text, (x - text_rect[2], y))
+
+
+def rectangle_text(pos: tuple, text: str, size: int, color: list) -> pg.Rect:
+    """
+    Fonction affichant un rectangle contenant du texte
+
+    Entrée: La position du rectangle (en partant de l'angle du haut à gauche + 10 pixels)
+            Le texte à afficher dans le rectangle
+            La taille de la police d'écriture
+    Sortie: Rect du rectangle affiché
+    """
+    shop_text = Font[size].render(text, 0, color)
+    shop_text_rect = shop_text.get_rect()
+
+    # On affiche le rectangle à doite puisqu'il y a de la place
+    if shop_text_rect[2] + pos[0] + 26 <= 1624:
+        pg.draw.rect(screen, BLACK, (pos[0] + 12, pos[1] + 2, shop_text_rect[2] + 11, 8 + shop_text_rect[3]))
+        rect = pg.draw.rect(screen, WHITE, (pos[0] + 10, pos[1], shop_text_rect[2] + 15, 10 + shop_text_rect[3]), width=3)
+        screen.blit(shop_text, (pos[0] + 15, pos[1] + 10))
+    # Il n'y a pas de place à droite, on le met donc à gauche
+    else:
+        pg.draw.rect(screen, BLACK, (pos[0] - shop_text_rect[2] - 22, pos[1] + 2, shop_text_rect[2] + 16, 8 + shop_text_rect[3]))
+        rect = pg.draw.rect(screen, WHITE, (pos[0] - shop_text_rect[2] - 25, pos[1], shop_text_rect[2] + 20, 10 + shop_text_rect[3]), width=3)
+        screen.blit(shop_text, (pos[0] - shop_text_rect[2] - 15, pos[1] + 10))
+    return rect
+
+
+def user_entry(nb='', numbers=True) -> str:
+    """
+    Fonction permettant de récupérer les entrées de l'utilisateur
+
+    Entrée: - variable sur lequelle sera concaténée ou écrasée la
+    valeur de l'entrée utilisateur
+            - bouléen pour avoir ou non les chiffres entrés par l'utilisateur
+    Sortie: entrées de l'utilisateur
+    """
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            quit()
+        elif event.type == pg.KEYDOWN:
+            if numbers:
+                if event.key == pg.K_KP0:
+                    nb += '0'
+                elif event.key == pg.K_KP1:
+                    nb += '1'
+                elif event.key == pg.K_KP2:
+                    nb += '2'
+                elif event.key == pg.K_KP3:
+                    nb += '3'
+                elif event.key == pg.K_KP4:
+                    nb += '4'
+                elif event.key == pg.K_KP5:
+                    nb += '5'
+                elif event.key == pg.K_KP6:
+                    nb += '6'
+                elif event.key == pg.K_KP7:
+                    nb += '7'
+                elif event.key == pg.K_KP8:
+                    nb += '8'
+                elif event.key == pg.K_KP9:
+                    nb += '9'
+                elif event.key == pg.K_BACKSPACE:
+                    nb = nb[:-1]
+            if (event.key == pg.K_RETURN or event.key == pg.K_KP_ENTER) and nb != '':
+                nb += '\n'
+                break
+            elif event.key == pg.K_ESCAPE:
+                nb = 'QUIT'
+                break
+            elif event.key == pg.K_RIGHT:
+                nb += 'NEXT'
+                break
+            elif event.key == pg.K_LEFT:
+                nb += 'PREVIOUS'
+                break
+    return nb
 
 
 def flourish_and_blotts(monnaie: int) -> dict:
@@ -217,58 +296,6 @@ def ollivander(monnaie:list) -> list:
     return monnaie_rendue
 
 
-def user_entry(nb='', numbers=True) -> str:
-    """
-    Fonction permettant de récupérer les entrées de l'utilisateur
-
-    Entrée: - variable sur lequelle sera concaténée ou écrasée la
-    valeur de l'entrée utilisateur
-            - bouléen pour avoir ou non les chiffres entrés par l'utilisateur
-    Sortie: entrées de l'utilisateur
-    """
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            quit()
-        elif event.type == pg.KEYDOWN:
-            if numbers:
-                if event.key == pg.K_KP0:
-                    nb += '0'
-                elif event.key == pg.K_KP1:
-                    nb += '1'
-                elif event.key == pg.K_KP2:
-                    nb += '2'
-                elif event.key == pg.K_KP3:
-                    nb += '3'
-                elif event.key == pg.K_KP4:
-                    nb += '4'
-                elif event.key == pg.K_KP5:
-                    nb += '5'
-                elif event.key == pg.K_KP6:
-                    nb += '6'
-                elif event.key == pg.K_KP7:
-                    nb += '7'
-                elif event.key == pg.K_KP8:
-                    nb += '8'
-                elif event.key == pg.K_KP9:
-                    nb += '9'
-                elif event.key == pg.K_BACKSPACE:
-                    nb = nb[:-1]
-            if (event.key == pg.K_RETURN or event.key == pg.K_KP_ENTER) and nb != '':
-                nb += '\n'
-                break
-            elif event.key == pg.K_ESCAPE:
-                nb = 'QUIT'
-                break
-            elif event.key == pg.K_RIGHT:
-                nb += 'NEXT'
-                break
-            elif event.key == pg.K_LEFT:
-                nb += 'PREVIOUS'
-                break
-    return nb
-
-
 def give_back(repaid: dict or list):
     """
     Fonction permettant d'afficher le rendu pour n'importe quelle boutique
@@ -295,6 +322,102 @@ def give_back(repaid: dict or list):
         display_text("Nothing !", Font, 70, GREEN, 812, 560 if type(repaid) == dict else 700, alignment=1)
 
 
+def display_furnitures(furnitures: list):
+    """
+    Fonction permettant l'affichage des fournitures mises dans la valise d'Harry Potter
+    avec le poids et le mana total
+
+    Entrée: La liste des fournitures dans la valise
+    """
+    if furnitures == {}:
+        return
+
+    display_text("We have put in Harry's trunk:", Font, 70, GREY, 20, 180, alignment=0)
+    weight, mana = 0, 0
+    for i, elt in enumerate(furnitures):
+        display_text('- 1 ' + elt["Nom"], Font, 60, BLACK, 80, 300 + i*80, alignment=0)
+        weight += elt["Poids"]
+        mana += elt["Mana"]
+        if i == len(furnitures) - 1:
+            display_text(f"Total mana: {mana}", Font, 60, BLUE, 40, 975, alignment=0)
+            display_text(f"Total weight: {weight}", Font, 60, RED, 525, 975, alignment=0)
+
+
+def all_possibilities(list):
+    n = len(list)
+    bin_list = []
+    for i in range(2**n):
+        nb_bin = bin(i)[2:]
+        nb_bin = (n - len(nb_bin)) * '0' + nb_bin
+        bin_list.append(nb_bin)
+
+    elts_combination = []
+    for str_bin in bin_list:
+        combi = []
+        for i, str in enumerate(str_bin):
+            if str == '1':
+                combi.append(list[i])
+        elts_combination.append(combi)
+
+    return elts_combination
+
+
+def brute_force_management(list, max_weight):
+    max_mana = 0
+    for elts in list:
+        weight = 0
+        mana = 0
+        for elt in elts:
+            weight += elt["Poids"]
+            mana += elt["Mana"]
+        if weight <= max_weight and max_mana < mana:
+            best = elts
+            max_mana = mana
+    return best
+
+
+def wanky_management(fournitures, poids_max):
+    malle_harry = []    
+
+    for element in fournitures:
+        if element['Poids'] <= poids_max :
+            malle_harry.append(element)
+            poids_max -= element['Poids']
+
+    return malle_harry
+
+
+def max_weight_management(fournitures):
+    poids_liste = []
+    for element in fournitures:
+        poids_liste.append(element["Poids"])
+    
+
+    for i in range (1, len(poids_liste)):
+        while poids_liste[i] < poids_liste[i - 1] and i > 0:
+            poids_liste[i], poids_liste[i - 1] = poids_liste[i - 1],\
+                poids_liste[i]
+            i = i - 1
+
+    somme_poids = 0
+    liste_poids = []
+    for i in poids_liste:
+        somme_poids += i
+        liste_poids.append(i)
+        if somme_poids > 4:
+            somme_poids -= i
+            liste_poids.remove(i)
+            break
+
+    malle_max = []
+    for element in SCHOLAR_FURNITURES:
+        for poids in liste_poids:
+            if poids == element["Poids"]:
+                malle_max.append(element)
+    
+    return(malle_max)
+
+
 def shop(shop: int):
     """
     Fonction permettant l'affichage et la gestion de n'importe quelle boutique
@@ -310,7 +433,7 @@ def shop(shop: int):
     money_list = (" euros", " galleons", " sickles", " knuts")
     previous_test = True
     previous_tests = [1]
-    malle = {}
+    trunk_content = {}
     if shop == 0:
         money_type = 0
         previous_tests += list(MALKIN_TESTS)
@@ -451,11 +574,11 @@ def shop(shop: int):
             if button_anything_rect.collidepoint(mouse_pos[0], mouse_pos[1]):
                 rectangle_text((1175, 290), "Take anything", 40, GREEN)
                 if mouse_down:
-                    malle = remplissage_pif_malle(fournitures_scolaires)
+                    trunk_content = wanky_management(SCHOLAR_FURNITURES, MAX_WEIGHT)
             elif button_weight_rect.collidepoint(mouse_pos[0], mouse_pos[1]):
                 rectangle_text((1175, 390), "Max weight", 40, GREEN)
                 if mouse_down:
-                    malle = remplissage_max(fournitures_scolaires)
+                    trunk_content = max_weight_management(SCHOLAR_FURNITURES)
             elif button_mana_rect.collidepoint(mouse_pos[0], mouse_pos[1]):
                 rectangle_text((1175, 490), "Max mana", 40, GREEN)
                 if mouse_down:
@@ -467,102 +590,11 @@ def shop(shop: int):
             elif button_best_rect.collidepoint(mouse_pos[0], mouse_pos[1]):
                 rectangle_text((1175, 690), "Best management", 40, GREEN)
                 if mouse_down:
-                    print("Best")
-            display_furnitures(malle)
+                    trunk_content = brute_force_management(all_possibilities(SCHOLAR_FURNITURES), MAX_WEIGHT)
+            display_furnitures(trunk_content)
 
         pg.display.update()
         clock.tick(FPS)
-
-
-def remplissage_pif_malle(fournitures):
-    malle_harry = []    
-    poids_max = 4
-    
-    for element in fournitures:
-        if element['Poids'] <= poids_max :
-            malle_harry.append(element)
-            poids_max -= element['Poids']
-
-    return malle_harry
-
-
-def remplissage_max(fournitures):
-    
-    poids_liste = []
-    for element in fournitures:
-        poids_liste.append(element["Poids"])
-    
-
-    for i in range (1, len(poids_liste)):
-        while poids_liste[i] < poids_liste[i - 1] and i > 0:
-            poids_liste[i], poids_liste[i - 1] = poids_liste[i - 1],\
-                poids_liste[i]
-            i = i - 1
-        
-
-    somme_poids = 0
-    liste_poids = []
-    for i in poids_liste:
-        somme_poids += i
-        liste_poids.append(i)
-        if somme_poids > 4:
-            somme_poids -= i
-            liste_poids.remove(i)
-            break
-
-    malle_max = []
-    for element in fournitures_scolaires:
-        for poids in liste_poids:
-            if poids == element["Poids"]:
-                malle_max.append(element)
-    
-    return(malle_max)
-
-
-def display_furnitures(furnitures: list):
-    """
-    Fonction permettant l'affichage des fournitures mises dans la valise d'Harry Potter
-    avec le poids et le mana total
-
-    Entrée: La liste des fournitures dans la valise
-    """
-    if furnitures == {}:
-        return
-
-    display_text("We can put in Harry's trunk :", Font, 70, GREY, 20, 180, alignment=0)
-    weight, mana = 0, 0
-    for i, elt in enumerate(furnitures):
-        display_text('- 1 ' + elt["Nom"], Font, 60, BLACK, 40, 300 + i*80, alignment=0)
-        weight += elt["Poids"]
-        mana += elt["Mana"]
-        if i == len(furnitures) - 1:
-            display_text(f"Total mana: {mana}", Font, 60, BLUE, 40, 975, alignment=0)
-            display_text(f"Total weight: {weight}", Font, 60, RED, 525, 975, alignment=0)
-
-
-def rectangle_text(pos: tuple, text: str, size: int, color: list) -> pg.Rect:
-    """
-    Fonction affichant un rectangle contenant du texte
-
-    Entrée: La position du rectangle (en partant de l'angle du haut à gauche + 10 pixels)
-            Le texte à afficher dans le rectangle
-            La taille de la police d'écriture
-    Sortie: Rect du rectangle affiché
-    """
-    shop_text = Font[size].render(text, 0, color)
-    shop_text_rect = shop_text.get_rect()
-
-    # On affiche le rectangle à doite puisqu'il y a de la place
-    if shop_text_rect[2] + pos[0] + 26 <= 1624:
-        pg.draw.rect(screen, BLACK, (pos[0] + 12, pos[1] + 2, shop_text_rect[2] + 11, 8 + shop_text_rect[3]))
-        rect = pg.draw.rect(screen, WHITE, (pos[0] + 10, pos[1], shop_text_rect[2] + 15, 10 + shop_text_rect[3]), width=3)
-        screen.blit(shop_text, (pos[0] + 15, pos[1] + 10))
-    # Il n'y a pas de place à droite, on le met donc à gauche
-    else:
-        pg.draw.rect(screen, BLACK, (pos[0] - shop_text_rect[2] - 22, pos[1] + 2, shop_text_rect[2] + 16, 8 + shop_text_rect[3]))
-        rect = pg.draw.rect(screen, WHITE, (pos[0] - shop_text_rect[2] - 25, pos[1], shop_text_rect[2] + 20, 10 + shop_text_rect[3]), width=3)
-        screen.blit(shop_text, (pos[0] - shop_text_rect[2] - 15, pos[1] + 10))
-    return rect
 
 
 def alley(mouse_pos: tuple, pressed: bool):
@@ -600,16 +632,10 @@ def alley(mouse_pos: tuple, pressed: bool):
         if pressed:
             shop(3)
 
-def update_fps():
-    font = pg.font.SysFont("Arial", 18)
-    fps = str(int(clock.get_fps()))
-    fps_text = font.render(fps, 1, pg.Color("coral"))
-    return fps_text
 
 def main():
     """
-    Fonction principale ne se terminant que si l'utilisateur
-    quitte la fenêtre ou appuie sur esc
+    Fonction principale bouclant en continu
     """
     while 1:
         mouse_pressed = False
